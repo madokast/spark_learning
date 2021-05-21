c8f3d723a05c30d30f131e20ebf13c47e31cde53
madokast <578562554@qq.com>
2021-05-21 12:50:55 +0800

    scala

--- shangguigu2021/atguigu-classes/scala-learning/src/com/sekai/HelloWorld.scala
+++ shangguigu2021/atguigu-classes/scala-learning/src/com/sekai/HelloWorld.scala
@@ -1,5 +1,12 @@
 package com.sekai
 
-class HelloWorld {
+object HelloWorld {
+  def main(args:Array[String]):Unit={
+    println("Hello, world!")
+    println(sayHello())
+    var fun = (x:Int) => x+1
+    println(fun(10))
+  }
 
+  def sayHello():String="Hello"
 }
