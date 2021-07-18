2226662698d0afbd14f824b838601261c5fabf91
madokast <578562554@qq.com>
2021-07-18 15:27:17 +0800

    wc

--- spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A01WordCount.scala
+++ spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A01WordCount.scala
@@ -8,7 +8,22 @@ object A01WordCount {
     val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
     // 操作
+    // 1. 读取文件，按行读取
+    sc.textFile("data/word_count.txt")
+      // 2. 将一行拆分为单词
+      .flatMap(_.split(" "))
+      // 3. 相同单词合并。(a,a,a)  (b,b)，即 group by self
+      .groupBy(word => word)
+      // 4. 分组后的数据，转换map 。(a,a,a)=>(a,3)  (b,b)=>(b,2)
+      .map(group => (group._1, group._2.size))
+      .collect()
+      // 5. 显示结果
+      .foreach(println)
 
+    //(spark,1)
+    //(hello,3)
+    //(world,2)
+    
     // 关闭链接
     sc.stop()
   }
