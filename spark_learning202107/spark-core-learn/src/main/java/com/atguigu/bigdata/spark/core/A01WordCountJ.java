ad63d227ddab7a1d7677da89e0e55a709003563c
madokast <578562554@qq.com>
2021-07-22 09:09:28 +0800

    0720

--- spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A01WordCountJ.java
+++ spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A01WordCountJ.java
@@ -2,11 +2,36 @@ package com.atguigu.bigdata.spark.core;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class A01WordCountJ {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("w");
-        SparkContext sparkContext = new SparkContext(sparkConf);
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        sparkContext.textFile("data/word_count.txt")
+                .flatMap(line-> Arrays.asList(line.split(" ")).iterator())
+                .groupBy(w->w)
+                .map(t->{
+                    Iterator<String> iter = t._2().iterator();
+//                    System.out.println(iter.getClass());
+                    int c = 0;
+                    while (iter.hasNext()){
+                        c++;
+                        iter.next();
+                    }
+                    return new Tuple2<String,Integer>(t._1,c);
+                })
+                .collect()
+                .forEach(System.out::println);
+
 
         sparkContext.stop();
     }
--- spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A02WordCountReduce.scala
+++ spark_learning202107/spark-core-learn/src/main/java/com/atguigu/bigdata/spark/core/A02WordCountReduce.scala
@@ -2,7 +2,7 @@ package com.atguigu.bigdata.spark.core
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object A01WordCount {
+object A02WordCountReduce {
   def main(args: Array[String]): Unit = {
     // 建立和 spark 的连接
     val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
@@ -12,13 +12,11 @@ object A01WordCount {
     sc.textFile("data/word_count.txt")
       // 2. 将一行拆分为单词
       .flatMap(_.split(" "))
-      // 3. 相同单词合并。(a,a,a)  (b,b)，即 group by self
-      .groupBy(word => word)
-      // 4. 分组后的数据，转换map 。(a,a,a)=>(a,3)  (b,b)=>(b,2)
-      //      .map(group => (group._1, group._2.size))
-      .map({ case (word, words) => (word, words.size) })
-      .collect()
-      // 5. 显示结果
+      // 3. 每个单词 map 为 (w,1)
+      .map((_, 1))
+      // 4. reduce by key
+      .reduceByKey(Integer.sum)
+      // 5. show
       .foreach(println)
 
     //(spark,1)
